221d2e3147b2002b43304c5fc3a427f742d1c3cf
Niraj Menon <nirajmmenon@gmail.com>
2019-09-18 10:32:59 -0400

    [DISPLAY] Changed delay to game board

--- public/js/splashscreen.js
+++ public/js/splashscreen.js
@@ -61,7 +61,7 @@ function waitForInput() {
     if (document.getElementById ("welcome").style ['opacity'] == '0')
     {
         document.getElementById ("welcome").style ['display'] = 'none'
-        id = setTimeout (showGameBoard, 500)
+        id = setTimeout (showGameBoard, 250)
     }
     else
     {
