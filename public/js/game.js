4caebf63f198fd60d3f5f99e681c296f01f8b2df
Niraj Menon <nirajmmenon@gmail.com>
2019-11-16 18:05:17 -0500

    [DISPLAY] Rewritten board with card data

---
+++ public/js/game.js
@@ -0,0 +1,364 @@
+properties = [{"name": "stjames",
+               "color": "#eb9e34",
+               "displayname":"St. James Place",
+               "rent": "$14",
+               "house1": "$70",
+               "house2": "$200",
+               "house3": "$550",
+               "house4": "$750",
+               "hotel": "$950",
+               "mortgage": "$90",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "tennessee",
+               "color": "#eb9e34",
+               "displayname":"Tennessee Ave.",
+               "rent": "$14",
+               "house1": "$70",
+               "house2": "$200",
+               "house3": "$550",
+               "house4": "$750",
+               "hotel": "$950",
+               "mortgage": "$90",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "newyork",
+               "color": "#eb9e34",
+               "displayname":"New York Ave.",
+               "rent": "$16",
+               "house1": "$80",
+               "house2": "$220",
+               "house3": "$600",
+               "house4": "$800",
+               "hotel": "$1000",
+               "mortgage": "$100",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "illinois",
+               "color": "#f2322c",
+               "displayname":"Illinois Ave.",
+               "rent": "$20",
+               "house1": "$100",
+               "house2": "$300",
+               "house3": "$750",
+               "house4": "$925",
+               "hotel": "$1100",
+               "mortgage": "$120",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              
+              {"name": "kentucky",
+               "color": "#f2322c",
+               "displayname":"Kentucky Ave.",
+               "rent": "$18",
+               "house1": "$90",
+               "house2": "$250",
+               "house3": "$700",
+               "house4": "$875",
+               "hotel": "$1050",
+               "mortgage": "$110",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              {"name": "connecticut",
+               "color": "#5bf2f5",
+               "displayname":"Connecticut Ave.",
+               "rent": "$8",
+               "house1": "$40",
+               "house2": "$100",
+               "house3": "$300",
+               "house4": "$450",
+               "hotel": "$600",
+               "mortgage": "$60",
+               "housecost": "$50",
+               "posthotel": "$50"
+              },
+              
+              {"name": "indiana",
+               "color": "#f2322c",
+               "displayname":"Indiana Ave.",
+               "rent": "$18",
+               "house1": "$90",
+               "house2": "$250",
+               "house3": "$700",
+               "house4": "$875",
+               "hotel": "$1050",
+               "mortgage": "$110",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              {"name": "boardwalk",
+               "color": "#024abf",
+               "displayname":"Boardwalk",
+               "rent": "$50",
+               "house1": "$200",
+               "house2": "$600",
+               "house3": "$1400",
+               "house4": "$1700",
+               "hotel": "$2000",
+               "mortgage": "$200",
+               "housecost": "$200",
+               "posthotel": "$200"
+              },
+              {"name": "vermont",
+               "color": "#5bf2f5",
+               "displayname":"Vermont Ave.",
+               "rent": "$6",
+               "house1": "$30",
+               "house2": "$90",
+               "house3": "$270",
+               "house4": "$400",
+               "hotel": "$550",
+               "mortgage": "$50",
+               "housecost": "$50",
+               "posthotel": "$50"
+              },
+              
+              {"name": "ventnor",
+               "color": "#fff769",
+               "displayname":"Ventnor Ave.",
+               "rent": "$22",
+               "house1": "$110",
+               "house2": "$330",
+               "house3": "$800",
+               "house4": "$975",
+               "hotel": "$1150",
+               "mortgage": "$130",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              
+              {"name": "atlantic",
+               "color": "#fff769",
+               "displayname":"Atlantic Ave.",
+               "rent": "$22",
+               "house1": "$110",
+               "house2": "$330",
+               "house3": "$800",
+               "house4": "$975",
+               "hotel": "$1150",
+               "mortgage": "$130",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              {"name": "stcharles",
+               "color": "#964bbf",
+               "displayname":"St. Charles Place",
+               "rent": "$10",
+               "house1": "$50",
+               "house2": "$150",
+               "house3": "$450",
+               "house4": "$625",
+               "hotel": "$750",
+               "mortgage": "$70",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "statesave",
+               "color": "#964bbf",
+               "displayname":"States Ave.",
+               "rent": "$10",
+               "house1": "$50",
+               "house2": "$150",
+               "house3": "$450",
+               "house4": "$625",
+               "hotel": "$750",
+               "mortgage": "$70",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "virginia",
+               "color": "#964bbf",
+               "displayname":"Virginia Ave.",
+               "rent": "$12",
+               "house1": "$60",
+               "house2": "$180",
+               "house3": "$500",
+               "house4": "$700",
+               "hotel": "$900",
+               "mortgage": "$80",
+               "housecost": "$100",
+               "posthotel": "$100"
+              },
+              {"name": "marvin",
+               "color": "#fff769",
+               "displayname":"Marvin Gardens",
+               "rent": "$24",
+               "house1": "$120",
+               "house2": "$360",
+               "house3": "$850",
+               "house4": "$1025",
+               "hotel": "$1200",
+               "mortgage": "$140",
+               "housecost": "$150",
+               "posthotel": "$150"
+              },
+              {"name": "oriental",
+               "color": "#5bf2f5",
+               "displayname":"Oriental Ave.",
+               "rent": "$6",
+               "house1": "$30",
+               "house2": "$90",
+               "house3": "$270",
+               "house4": "$400",
+               "hotel": "$550",
+               "mortgage": "$50",
+               "housecost": "$50",
+               "posthotel": "$50"
+              },
+              {"name": "baltic",
+               "color": "#964bbf",
+               "displayname":"Baltic Ave.",
+               "rent": "$4",
+               "house1": "$20",
+               "house2": "$60",
+               "house3": "$180",
+               "house4": "$320",
+               "hotel": "$450",
+               "mortgage": "$30",
+               "housecost": "$50",
+               "posthotel": "$50"
+              },
+              {"name": "parkplace",
+               "color": "#024abf",
+               "displayname":"Park Place",
+               "rent": "$35",
+               "house1": "$175",
+               "house2": "$500",
+               "house3": "$1100",
+               "house4": "$1300",
+               "hotel": "$1500",
+               "mortgage": "$175",
+               "housecost": "$200",
+               "posthotel": "$200"
+              },
+              {"name": "mediterranean",
+               "color": "#964bbf",
+               "displayname":"Mediterranean Ave.",
+               "rent": "$2",
+               "house1": "$10",
+               "house2": "$30",
+               "house3": "$90",
+               "house4": "$160",
+               "hotel": "$250",
+               "mortgage": "$30",
+               "housecost": "$50",
+               "posthotel": "$50"
+              },
+              {"name": "northcarolina",
+               "color": "#00b04f",
+               "displayname":"North Carolina Ave.",
+               "rent": "$26",
+               "house1": "$130",
+               "house2": "$390",
+               "house3": "$900",
+               "house4": "$1100",
+               "hotel": "$1275",
+               "mortgage": "$150",
+               "housecost": "$200",
+               "posthotel": "$200"
+              },
+              {"name": "pennsylvania",
+               "color": "#00b04f",
+               "displayname":"Pennsylvania Ave.",
+               "rent": "$28",
+               "house1": "$150",
+               "house2": "$450",
+               "house3": "$1000",
+               "house4": "$1200",
+               "hotel": "$1400",
+               "mortgage": "$160",
+               "housecost": "$200",
+               "posthotel": "$200"
+              },
+              {"name": "pacific",
+               "color": "#00b04f",
+               "displayname":"Pacific Ave.",
+               "rent": "$26",
+               "house1": "$130",
+               "house2": "$390",
+               "house3": "$900",
+               "house4": "$1100",
+               "hotel": "$1275",
+               "mortgage": "$150",
+               "housecost": "$200",
+               "posthotel": "$200"
+              }
+            ]
+
+unique_props = [
+                {"name": "waterworks",
+                    "displayname":"Water Works",
+                    "mortgage": "75",
+                },
+                {"name": "electricco",
+                    "displayname":"Electric Co.",
+                    "mortgage": "75",
+                },
+                {"name": "readingrr"},
+                {"name": "pennsylvaniarr"},
+                {"name": "borailroad"},
+                {"name": "shortlinerr"},
+                ]
+
+                stjames
+                tennessee
+                newyork
+                illinois
+                kentucky
+                connecticut
+                indiana
+                boardwalk
+                vermont
+                ventnor
+                atlantic
+                stcharles
+                statesave
+                virginia
+                marvin
+                oriental
+                baltic
+                parkplace
+                mediterranean
+                northcarolina
+                pennsylvania
+                pacific
+                waterworks
+                electricco
+                readingrr
+                pennsylvaniarr
+                borailroad
+                shortlinerr
+
+railway = [
+            ["Rent", 25],  //rent 
+            ["Own 2 railroads", 50],  //own 2 rr's
+            ["Own 3 railroads", 100], //own 3 rr's 
+            ["Own 4 railroads", 200], //own 4 rr's 
+            ["Mortage Value", 100], //mortgage
+          ];
+
+$("#diedialog").css ('display', 'flex')
+        $("#landed_unowned_dialog").css ('display', 'none')
+        $("#overlay").css ('opacity', 1)
+        setTimeout (function () {
+            $("#rolldie").attr ("src", "public/images/die.gif")
+            $("#dialog1").innerHTML = "Dice roll detected!"
+            $("#dialog2").innerHTML = "Waiting for roll..."
+            setTimeout (function () {
+                $("#rolldie").src = "public/images/die.png"
+                $("#dialog1").innerHTML = "Dice roll received!"
+                $("#dialog2").innerHTML = "Roll was 9!"
+            setTimeout (landed_on_tile, 2000);
+            }, 2000)
+        }, 2000)
+    function landed_on_tile () {
+        $("#overlay").style ['opacity'] = 0;
+        $("#diedialog").style ['display'] = 'none';
+        $("#landed_unowned_dialog").style ['display'] = 'flex';
+        $("#overlay").style ['opacity'] = 1;
+     }
